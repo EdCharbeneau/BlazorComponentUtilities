911c0f500b3b8c595892bbfc7350ad469cc71592
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-02-20 13:34:54 -0500

    Replaced StringBuilder with a simple string. Most operations should only be a few strings and likely wouldn't see any performance benefit of using StringBuilder. Possible that simple concat is faster in this scenario. RE: http://jonskeet.uk/csharp/stringbuilder.html

--- Components.Utilities.Tests/CssBuilderTests.cs
+++ Components.Utilities.Tests/CssBuilderTests.cs
@@ -24,5 +24,26 @@ namespace Components.Utilities.Tests
             //assert
             ClassToRender.Should().Be("item-one item-three item-four");
         }
+        [Fact]
+        public void ShouldBulidConditionalCssBuilderClasses()
+        {
+            //arrange
+            var hasTwo = false;
+            var hasThree = true;
+            Func<bool> hasFive = () => false;
+
+            //act
+            var ClassToRender = new CssBuilder("item-one")
+                            .AddClass("item-two", when: hasTwo)
+                            .AddClass(new CssBuilder("item-three")
+                                            .AddClass("item-foo", false)
+                                            .AddClass("item-sub-three"), 
+                                            when: hasThree)
+                            .AddClass("item-four")
+                            .AddClass("item-five", when: hasFive)
+                            .Build();
+            //assert
+            ClassToRender.Should().Be("item-one item-three item-sub-three item-four");
+        }
     }
 }
--- Components.Utilities/CssBuilder.cs
+++ Components.Utilities/CssBuilder.cs
@@ -5,20 +5,20 @@ namespace Components.Utilities
 {
     public class CssBuilder
     {
-        private readonly StringBuilder sb;
+        private string stringBuffer;
 
         /// <summary>
         /// Creates a CssBuilder used to define conditional CSS classes used in a component.
         /// Call Build() to return the completed CSS Classes as a string. 
         /// </summary>
-        public CssBuilder() => sb = new StringBuilder();
+        public CssBuilder() { }
 
         /// <summary>
         /// Creates a CssBuilder used to define conditional CSS classes used in a component.
         /// Call Build() to return the completed CSS Classes as a string. 
         /// </summary>
         /// <param name="value"></param>
-        public CssBuilder(string value) => sb = new StringBuilder(value);
+        public CssBuilder(string value) => stringBuffer = value;
 
         /// <summary>
         /// Adds a raw string to the builder that will be concatinated with the next class or value added to the builder.
@@ -27,7 +27,7 @@ namespace Components.Utilities
         /// <returns>CssBuilder</returns>
         public CssBuilder AddValue(string value)
         {
-            sb.Append(value);
+            stringBuffer += value;
             return this;
         }
 
@@ -55,11 +55,27 @@ namespace Components.Utilities
         public CssBuilder AddClass(string value, Func<bool> when = null) => this.AddClass(value, when());
 
         /// <summary>
+        /// Adds a conditional CSS Class to the builder with space seperator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Conditon in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(CssBuilder builder, bool when = true) => when ? this.AddClass(builder.ToString()) : this;
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space seperator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Conditon in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(CssBuilder builder, Func<bool> when = null) => this.AddClass(builder.ToString(), when());
+
+        /// <summary>
         /// Finalize the completed CSS Classes as a string.
         /// </summary>
         /// <returns>string</returns>
         public string Build() => ToString();
-        public override string ToString() => sb.ToString();
+        public override string ToString() => stringBuffer.Trim();
 
     }
 }
