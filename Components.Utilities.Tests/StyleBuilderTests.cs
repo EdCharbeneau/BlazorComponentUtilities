5b2928958c7f891985416323dcd96181c5fd94bd
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-02-22 17:46:53 -0500

    Prototype style builder.

---
+++ Components.Utilities.Tests/StyleBuilderTests.cs
@@ -0,0 +1,49 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Components.Utilities.Tests
+{
+    public class CssBuilderTests
+    {
+        [Fact]
+        public void ShouldBulidConditionalCssClasses()
+        {
+            //arrange
+            var hasTwo = false;
+            var hasThree = true;
+            Func<bool> hasFive = () => false;
+
+            //act
+            var ClassToRender = new CssBuilder("item-one")
+                            .AddClass("item-two", when: hasTwo)
+                            .AddClass("item-three", when: hasThree)
+                            .AddClass("item-four")
+                            .AddClass("item-five", when: hasFive)
+                            .Build();
+            //assert
+            ClassToRender.Should().Be("item-one item-three item-four");
+        }
+        [Fact]
+        public void ShouldBulidConditionalCssBuilderClasses()
+        {
+            //arrange
+            var hasTwo = false;
+            var hasThree = true;
+            Func<bool> hasFive = () => false;
+
+            //act
+            var ClassToRender = new CssBuilder("item-one")
+                            .AddClass("item-two", when: hasTwo)
+                            .AddClass(new CssBuilder("item-three")
+                                            .AddClass("item-foo", false)
+                                            .AddClass("item-sub-three"), 
+                                            when: hasThree)
+                            .AddClass("item-four")
+                            .AddClass("item-five", when: hasFive)
+                            .Build();
+            //assert
+            ClassToRender.Should().Be("item-one item-three item-sub-three item-four");
+        }
+    }
+}
